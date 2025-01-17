3d7916d74d95b0689730b0cf9b73073cbaa44c5b
Mohammed Siddique <mohammed.siddique@mgmforgings.com>
2025-01-17 10:57:28 +0530

    Counter_app_js
    Counter application for logging entries

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-// document.getElementById("count-el")
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.querySelector("#save-el")
 
+// increments the count when clicked increment button
 let count = 0
 function increment() {
     count+=1
-    countEl.innerHTML = count
+    countEl.textContent = count
 }
 
+// saves the incremented values and initializes the count to zero
 function save() {
     console.log(count)
+    saveEl.textContent += " "+count+" - "
+    countEl.textContent = 0 
+    count = 0
 }
+
+
+
+
+
+
 
  
